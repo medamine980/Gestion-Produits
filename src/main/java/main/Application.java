47b52d58fac0ddb2e2ff2722549a04811effa53f
Mohamed-Amine Benali <benali.medamine2002@gmail.com>
2024-01-11 00:02:15 +0100

    Réparation (correction de bug) de la méthode Ajouter dans la classe ProduitService

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	public static void main(String[] args) {
 		
 	 ProduitService service =new ProduitService();	
-	 Produit produit = new Produit(1,"tonic",1.33,12);
+	 Produit produit = new Produit(1,"tonic",-1.33,12);
 	 Produit produit2 = new Produit(2,"Tide",1.45,11);
 	 Produit produit3 = new Produit(3,"Tide",1.23,12);
 	 service.Ajouter(produit3);
